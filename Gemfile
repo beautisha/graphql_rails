ff48bdf8d4814017adc87644caf175c5228ec349
beautisha <korobochky@yandex.ru>
2025-12-14 15:42:42 +0300

    final

--- Gemfile
+++ Gemfile
@@ -48,4 +48,9 @@ group :development, :test do
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+
+  group :development do
+    gem "graphiql-rails"
+    gem "sprockets-rails"
+  end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,12 +1,35 @@
 # frozen_string_literal: true
 
 module Types
+  # Мутации отвечают за изменение данных через GraphQL.
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    # Создание нового курорта с обязательными атрибутами.
+    field :create_resort, Types::ResortType, null: false do
+      description "Creates new Resort"
+      argument :name, String, required: true
+      argument :email, String, required: true
+      argument :description, String, required: false
+    end
+
+    def create_resort(name:, email:, description: nil)
+      Resort.create!(name: name, email: email, description: description)
+    end
+
+    # Добавление апартаментов к существующему курорту.
+    field :create_apartment, Types::ApartmentType, null: false do
+      description "Creates new Apartment"
+      argument :resort_id, ID, required: true, camelize: false
+      argument :room_type, String, required: true, camelize: false
+      argument :description, String, required: false
+    end
+
+    def create_apartment(resort_id:, room_type:, description: nil)
+      Apartment.create!(
+        resort_id: resort_id,
+        room_type: room_type,
+        description: description
+      )
     end
   end
 end
+
